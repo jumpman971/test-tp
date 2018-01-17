50124935ec1e4b17612b821e2cf46d5cd3137900
taka <sandy.epiard@u-psud.fr>
2018-01-17 16:57:00 +0100

    test incudes

--- test/unit/IntervalTest.js
+++ test/unit/IntervalTest.js
@@ -1,4 +1,4 @@
-/*describe("Interval - overlapping", function () {
+describe("Interval - overlapping", function () {
     testedInterval = new Interval(10, 20);
 
     [
@@ -23,4 +23,31 @@
             expect(testedInterval.overlaps(interval)).toBeFalsy();
         });
     });
-});*/
+});
+
+describe("Interval - including", function () {
+    testedInterval = new Interval(10, 20);
+
+    [
+        
+        new Interval(15, 16),
+        new Interval(12, 19),
+        new Interval(10, 20)
+    ].forEach(function (interval) {
+        it("should includes " + testedInterval.toString() + " and " + interval.toString(), function () {
+            expect(testedInterval.includes(interval)).toBeTruthy();
+        });
+    });
+
+    [
+		new Interval(8, 12),
+		new Interval(17, 22),
+        new Interval(8, 9),
+		new Interval(8, 21),
+        new Interval(21, 22)
+    ].forEach(function (interval) {
+        it("should not includes " + testedInterval.toString() + " and " + interval.toString(), function () {
+            expect(testedInterval.includes(interval)).toBeFalsy();
+        });
+    });
+});
