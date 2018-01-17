4cd7d362cca8674d8e7b44f56a25a8229e9ed75b
taka <sandy.epiard@u-psud.fr>
2018-01-17 17:21:54 +0100

    test union

--- test/unit/IntervalTest.js
+++ test/unit/IntervalTest.js
@@ -51,3 +51,48 @@ describe("Interval - including", function () {
         });
     });
 });
+
+describe("Interval - union", function () {
+    testedInterval = new Interval(10, 20);
+
+    [
+		{
+			interval : new Interval(8, 12),
+			result : new Interval(8,20)
+		},
+		{
+			interval : new Interval(17, 22),
+			result : new Interval(10,22)
+		},
+		{
+			interval : new Interval(8, 9),
+			result : new Interval(8,20)
+		},
+		{
+			interval : new Interval(8, 21),
+			result : new Interval(8,21)
+		},
+		{
+			interval : new Interval(21, 22),
+			result : new Interval(10,22)
+		},
+		{
+			interval : new Interval(15, 16),
+			result : new Interval(10,20)
+		},
+		{
+			interval : new Interval(12, 19),
+			result : new Interval(10,20)
+		},
+		{
+			interval : new Interval(10, 20),
+			result : new Interval(10,20)
+		}
+    ].forEach(function (interval) {
+        it("should returns Interval[" + interval.result.start + ", " + interval.result.end + "] when doing union of " + testedInterval.toString() + " and " + interval.interval.toString(), function () {
+            var result = testedInterval.union(interval.interval);
+			expect(result.start).toEqual(interval.result.start);
+			expect(result.end).toEqual(interval.result.end);
+        });
+    });
+});
